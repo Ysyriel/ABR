86972fc1cf7349458d7aaf82118613a4a7bf124c
Pierre Caulet <pierre.caulet78@gmail.com>
2017-01-09 15:08:29 +0100

    On repart a 0. Zoubi

--- ABR.h
+++ ABR.h
@@ -1,11 +1,1 @@
-class Noeud {
-	private :
-		Noeud* fg;
-		Noeud* fd;
-		int cle;
-	public :
-		Noeud();
-		Noeud(Noeud* param1, Noeud* param2, int cle);
-		void recherche(int* cle, Noeud* racine);
-		int max(Noeud* racine);
-}
+
